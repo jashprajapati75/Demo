ca5bde865c9829d289cac938c00bcf4dc261046e
jashP <jalajprajapati7599@gmail.com>
2025-04-20 18:26:18 +0530

    edit

--- r11/src/App.jsx
+++ r11/src/App.jsx
@@ -3,15 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Workspace from './components/Workspace'
+import { MdDashboard } from "react-icons/md";
 
 function App() {
   return (
     <>
     <div className='bg-body-secondary w-25 vh-100 '>
      <ul className='list-group position-relative'>
-      <li className='mt-5'><Workspace/></li>
-      <li>Menu</li>
-      <li>Dashboard</li>
+      <li className='mt-5 position-relative'><Workspace/></li>
+      <li>
+        <i></i>
+        Menu</li>
+      <li>
+        <i><MdDashboard />
+        </i>
+        Dashboard</li>
      </ul>
      </div>
     </>
--- r11/src/components/Workspace.jsx
+++ r11/src/components/Workspace.jsx
@@ -9,6 +9,7 @@ import {
   FormGroup,
   Label,
 } from "reactstrap";
+
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 function Workspace() {
@@ -43,54 +44,95 @@ function Workspace() {
   const handleSelected = (w) => {
     setSelectedItem(w.name);
   };
+  const filterWorkspace = workspaceArr.filter((ws)=>(ws.name.toLowerCase().includes(searchQuery.toLowerCase())))
   return (
     <>
       <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle
-  caret={false}  
-  size="lg"
-  className="bg-white text-dark w-100 d-flex align-items-center justify-content-between border-primary border-2"
->
-  <div className="d-flex align-items-center gap-2 ">
-    <div className="bg-primary bg-opacity-25 ps-2 pe-2 p-1 border border-primary rounded text-primary">
-      <h3 className="m-0">
-        {selectedItem ? selectedItem[0]?.toUpperCase() : "W"}
-      </h3>
-    </div>
-    <span className="fw-bold">{selectedItem || "Workspace"}</span>
-  </div>
+        <DropdownToggle
+          caret={false}
+          size="lg"
+          className="bg-white text-dark w-100 d-flex align-items-center justify-content-between border-primary border-2"
+        >
+          <div className="d-flex align-items-center gap-2 ">
+            <div className="bg-primary bg-opacity-25 ps-2 pe-2 p-1 border border-primary rounded text-primary">
+              <h3 className="" style={{ minWidth: "2rem", minHeight: "1rem" }}>
+                {selectedItem ? selectedItem[0]?.toUpperCase() : "W"}
+              </h3>
+            </div>
+            <span className="fw-bold">{selectedItem || "Workspace"}</span>
+          </div>
 
-  <span className="dropdown-caret">&#9662;</span>
-</DropdownToggle>
+          <span className="dropdown-caret">
+            <i class="bi bi-arrow-down-up">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-down-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5m-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5"
+                />
+              </svg>
+            </i>
+          </span>
+        </DropdownToggle>
         <DropdownMenu
-          className="position-relative"
-          style={{ minWidth: "20rem" }}
+          className="position-absolute"
+          style={{ minWidth: "30rem", marginTop: "5rem" }}
         >
           <DropdownItem header>
-            <Input
-              type="text"
-              placeholder="search here"
-              name="search"
-              value={searchQuery}
-              onChange={handleSearchQueryChange}
-            />
+            <div className="position-relative">
+              <Input
+                type="text"
+                placeholder="Search"
+                name="search"
+                value={searchQuery}
+                onChange={handleSearchQueryChange}
+                className="ps-5"
+              />
+              <i
+                className="bi bi-search position-absolute top-50 start-0 translate-middle-y ms-3 text-muted"
+                style={{ pointerEvents: "none" }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+              </i>
+            </div>
           </DropdownItem>
-          {workspaceArr.length > 0
-            ? workspaceArr.map((Workspace) => (
-                <DropdownItem onClick={() => handleSelected(Workspace)}   className={
+          {filterWorkspace.length > 0
+            ? filterWorkspace.map((Workspace) => (
+                <DropdownItem
+                  onClick={() => handleSelected(Workspace)}
+                  className={
                     selectedItem === Workspace.name
                       ? "bg-primary text-black bg-opacity-25 border border-primary p-3 rounded"
                       : "p-3"
-                  }>
+                  }
+                >
                   <div className="d-flex">
-                    <div className="bg-primary bg-opacity-25  ps-2 pe-2 p-1 border border-primary rounded text-primary">
+                    <div
+                      className="bg-primary bg-opacity-25 border border-primary rounded text-primary d-flex justify-content-center align-items-center"
+                      style={{ width: "2.5rem", height: "2.5rem" }}
+                    >
                       <h3 className="">{Workspace.name[0].toUpperCase()}</h3>
                     </div>
                     &nbsp;&nbsp;
                     <div>{Workspace.name}</div>
                     {selectedItem === Workspace.name && (
-            <span className="ms-5">&#10003;</span> 
-        )}
+                      <span className="ms-5">&#10003;</span>
+                    )}
                   </div>
                 </DropdownItem>
               ))
@@ -103,7 +145,19 @@ function Workspace() {
                   onClick={toggle1}
                   className="border w-100"
                 >
-                  Create New Workspace
+                  <i class="bi bi-plus-square-fill">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-plus-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0" />
+                    </svg>
+                  </i>{" "}
+                  &nbsp;&nbsp; Create New Workspace
                 </Button>
                 <Modal isOpen={modal} fade={false} toggle={toggle1}>
                   <ModalHeader toggle={toggle1}>New Workspace</ModalHeader>
